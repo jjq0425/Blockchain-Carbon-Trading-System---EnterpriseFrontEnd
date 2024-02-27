d32aaf4fbe4edb4b0e08acc5c8350888f6b1513d
jjq419875574 <90457190+jjq419875574@users.noreply.github.com>
2024-02-27 11:21:48 +0000

    Deploying to gh-pages from  @ f605d518462845381284356a646246fdd7b9e2bf 🚀

--- js/lang-zh-CN-modal.80321dcc.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self["webpackChunkvue_antd_pro"]=self["webpackChunkvue_antd_pro"]||[]).push([[238],{24107:function(t,n,e){e.r(n),n["default"]={"modal.notice.title":"提示","modal.confirm.title":"请确认","modal.btn.cancel":"取消","modal.btn.confirm":"确定","modal.btn.ok2":"好的"}}}]);
