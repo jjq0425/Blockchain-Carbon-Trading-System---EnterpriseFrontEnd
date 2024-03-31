dbc55cc19196320ca07c79caf9004eb365ed0bb1
jjq <419875574@qq.com>
2024-03-31 23:49:33 +0800

    1

--- src/src/views/info/infoSubmission/infoSubmissionStep/SangJiGraph.vue
+++ src/src/views/info/infoSubmission/infoSubmissionStep/SangJiGraph.vue
@@ -58,7 +58,7 @@ export default {
             //       name: item.children[i].EmissionFactor[j].dataName,
             //     })
             // }
-            //
+            ///
           }
         }
       })
